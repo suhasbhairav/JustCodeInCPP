947b36f7045ab1e785788c5c9ebb024182932773
Suhas Bhairav <suhasbhairav@gmail.com>
2016-02-11 00:01:41 +0100

    Switch Statement

---
+++ JustCodeInCPP/switch.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	char a = 'c';
+	switch(a){
+		case 'a':
+			cout<< "Char A"<< endl;
+			break;
+		case 'c':
+			cout << "Char C"<< endl;
+			break;
+		default:
+			cout << "Other Chars"<<endl;
+	}
+
+}
